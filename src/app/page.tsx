6ca0e9f59fcaa6dcc22dcde7166674268726f88a
zedems01 <nguessie.achille@gmail.com>
2025-10-17 14:44:44 +0200

    docs update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our docs :)
           </a>
         </div>
       </main>
